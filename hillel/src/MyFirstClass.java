c0142d3091e6911c85b5948909f143c6e0bb8c60
OGlovatskaya <olex_88@mail.ru>
2015-09-17 21:19:58 +0300

    Hello world commit

---
+++ hillel/src/MyFirstClass.java
@@ -0,0 +1,26 @@
+/**
+ * Created by OGlovatskaya on 17.09.2015.
+ */
+public class MyFirstClass {
+    public static void main(String[] args) {
+        String helloMessage = "Hello world";
+        System.out.println(helloMessage);
+
+        final int maxBirthYear = 1984;
+
+        int age = 31;
+
+        System.out.println("Before changes" + maxBirthYear);
+
+        //maxBirthYear = 2015;
+
+        System.out.println("I'm " + age);
+
+
+        System.out.println("31" + 31);
+        System.out.println(31 +31);
+        System.out.println("After changes" + maxBirthYear);
+
+    }
+
+}
