614b1b178a2ef2d899f9b26c2fcb6a605787f8e5
OGlovatskaya <yow01919>
2015-09-18 22:05:02 +0300

    About myself

---
+++ hillel/src/AboutMyself.java
@@ -0,0 +1,16 @@
+/**
+ * Created by user on 18.09.2015.
+ */
+public class AboutMyself {
+    public static void main(String[] args) {
+        String myName = "Olga";
+        String mySurname = "Glovatskaya";
+        System.out.println(myName + " " + mySurname);
+
+        int birthYear = 1988;
+        int presentYear = 2015;
+        int myAge = presentYear - birthYear;
+        System.out.print("I'm " + myAge + " years old");
+    }
+
+    }
