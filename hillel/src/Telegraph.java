e7d417c704d4c7b83f9fa097fcc5f05d98060b1e
OGlovatskaya <yow01919>
2015-10-04 23:20:37 +0300

    Telegraph

--- hillel/src/Telegraph.java
+++ hillel/src/Telegraph.java
@@ -1,14 +1,20 @@
+import java.util.Scanner;
+
 /**
  * Created by user on 04.10.2015.
  */
 public class Telegraph {
     public static void main(String[] args ){
 
-        String message = "Hello world";
-        int price = 14;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the message: ");
+        String message = scanner.nextLine();
 
-        String[] splittedMessage = message.split(" ");
-        int messageLength = splittedMessage.length;
+        System.out.println("Enter the price for a word: ");
+        double price = scanner.nextDouble();
+
+        String[] splMessage = message.split(" ");
+        int messageLength = splMessage.length;
 
         double priceForTheMessage = price * messageLength;
         System.out.println(priceForTheMessage);
