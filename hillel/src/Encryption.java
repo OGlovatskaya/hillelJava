f526921e3d928635efd0e392bb6507e576e737fd
OGlovatskaya <yow01919>
2015-10-05 01:08:26 +0300

    Encryption

--- hillel/src/Encryption.java
+++ hillel/src/Encryption.java
@@ -8,17 +8,28 @@ public class Encryption {
         char key = 'f';
         System.out.println("Original message: " + message);
         System.out.println("Encrypted message: " + crypt(message, key));
+        System.out.println("Decrypted message: " + decrypt(message, key));
     }
 
     public static String crypt(String message, char key) {
-
         char[] cryptedMessage = message.toCharArray();
 
         for (int i = 0; i < cryptedMessage.length; i++) {
             cryptedMessage[i] = ((char) (cryptedMessage[i] ^ key));
         }
-        return new String(cryptedMessage);
-
+        String cryptMessage = String.copyValueOf(cryptedMessage);
+        return cryptMessage;
     }
+
+    public static String decrypt(String message, char key) {
+        char[] unCryptedtMassage = crypt(message, key).toCharArray();
+
+        for (int i = 0; i < unCryptedtMassage.length; i++) {
+            unCryptedtMassage[i] = ((char) (unCryptedtMassage[i] ^ key));
+        }
+        String unCryptMessage = String.copyValueOf(unCryptedtMassage);
+        return unCryptMessage;
+    }
+
 
 }
