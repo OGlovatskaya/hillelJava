ded8b2cd3b9b778e9cc5865642f6a728efc6f459
OGlovatskaya <yow01919>
2015-10-11 12:49:09 +0300

    ArtificialIntelligence

--- hillel/src/ArtificialIntelligence.java
+++ hillel/src/ArtificialIntelligence.java
@@ -7,31 +7,26 @@ import java.util.Scanner;
 public class ArtificialIntelligence {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String answer;
+
         Random random = new Random();
         int maxNumber = 10;
         int minNumber = 0;
         int number = random.nextInt(maxNumber - minNumber) + minNumber;
 
         System.out.println("Guess some number and remember it");
-        System.out.println("Did you guess " + number + "?");
 
-        while (scanner.hasNext()) {
-            String answer = scanner.nextLine();
+        do {
+            System.out.println("Did you guess " + number + "?");
+            answer = scanner.nextLine();
             if (answer.equals("down")) {
                 maxNumber = number;
                 number = random.nextInt(maxNumber - minNumber) + minNumber;
-                System.out.println("Did you guess " + number + "?");
             } else if (answer.equals("up")) {
                 minNumber = number + 1;
                 number = random.nextInt(maxNumber - minNumber) + minNumber;
-                System.out.println("Did you guess " + number + "?");
-            } else if (answer.equals("yes")) {
-                System.out.println("I guessed it!");
-                break;
             }
-        }
-
+        } while (!answer.equals("yes"));
+        System.out.println("I guessed it!");
     }
 }
-
-
