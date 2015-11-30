963ff7a676c3f34eb0b0891ee880bcce3a67b596
OGlovatskaya <olex_88@mail.ru>
2015-11-30 21:01:55 +0200

    Multithreading

---
+++ hillel/src/Multithreading/Bank/Bankier.java
@@ -0,0 +1,34 @@
+package Multithreading.Bank;
+
+import java.util.Random;
+
+/**
+ * Created by OGlovatskaya on 30.11.2015.
+ */
+public class Bankier extends Thread{
+    private Bank bank;
+    private Random random = new Random();
+    private long transactionCount;
+
+
+    public Bankier(Bank bank) {
+        this.bank = bank;
+    }
+
+    @Override
+    public void run() {
+        while (!Thread.currentThread().isInterrupted()){
+            transactionCount++;
+            int randomAmount = random.nextInt(1000);
+            boolean randomDirection = random.nextBoolean();
+            bank.transaction(randomAmount, randomDirection);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Bankier{" +
+                "transactionCount=" + transactionCount +
+                '}';
+    }
+}
---
+++ hillel/src/Multithreading/MultithreadingMain.java
@@ -0,0 +1,54 @@
+package Multithreading;
+
+/**
+ * Created by OGlovatskaya on 30.11.2015.
+ */
+public class MultithreadingMain {
+    public static void main(String[] args) {
+
+        Thread otherThread = new Thread(() -> {
+            System.out.println("other thread begin");
+
+            /*try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                System.out.println("other thread is interrupted");
+            }*/
+
+            while (! Thread.currentThread().isInterrupted()){
+
+            }
+            /*if (! Thread.currentThread().isInterrupted()){
+                return;
+            }*/
+            System.out.println("other thread end");
+        });
+
+        //otherThread.setDaemon(true);
+        otherThread.start();
+
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        otherThread.interrupt();
+
+        try {
+            otherThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("main thread end");
+
+
+        /*new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("other otherThread");
+            }
+        });*/
+
+    }
+}
