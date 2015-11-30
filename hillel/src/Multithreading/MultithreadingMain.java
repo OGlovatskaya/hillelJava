87d1f500a5d2db5c9c7be2e0c6165ef32e7beb1f
OGlovatskaya <olex_88@mail.ru>
2015-11-30 21:04:30 +0200

    Multithreading

--- hillel/src/Multithreading/MultithreadingMain.java
+++ hillel/src/Multithreading/MultithreadingMain.java
@@ -30,6 +30,7 @@ public class MultithreadingMain {
         try {
             Thread.sleep(1500);
         } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
             e.printStackTrace();
         }
 
